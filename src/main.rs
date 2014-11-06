2820169dc2974f4b9ae5d89625330b63e63039c5
Geoffroy Couprie <geo.couprie@gmail.com>
2014-11-06 15:47:23 +0100

    implement map, mapf and then

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #![feature(macro_rules)]
-use nom::{feed, parse, FileProducer, print, Error, Done, Incomplete};
+#![feature(unboxed_closures)]
+use nom::{FileProducer, print, Error, accline, Done, Incomplete, parse, feed};
 use std::str;
 
 mod nom;
@@ -8,29 +9,20 @@ fn main() {
   println!("Hello world!");
   let v = vec![3, 2, 1];
   match parse(v.as_slice()) {
-    Error(_, e)   => println!("error: {}", e),
+    Error(e)   => println!("error: {}", e),
     Done(i, o)    => println!("done: {}, rest: {}", o, i),
     Incomplete(_) => println!("incomplete")
   }
-
   feed();
   let v2 = "abc";
   print(&v2);
 
-  FileProducer::new("links.txt", 1024).map(|producer| {
+  FileProducer::new("links.txt", 1024).map(|producer: FileProducer| {
     let mut p = producer;
     loop {
-      match p.produce() {
-        Error(_, e)   => {
-          println!("error: {}", e);
-          break;
-        },
-        Done(i, o)    => println!("done: {}, rest: {}", str::from_utf8(o.as_slice()), i),
-        Incomplete(f) => {
-          println!("incomplete");
-        }
-      }
+      p.produce().mapf(|v| str::from_utf8(v.as_slice())).map(print);
     }
+    ()
   });
 
 }
--- src/nom.rs
+++ src/nom.rs
@@ -1,7 +1,9 @@
+#![feature(unboxed_closures)]
 #![desc = "Omnomnom incremental byte parser"]
 #![license = "MIT"]
 
 extern crate collections;
+
 
 use std::fmt::Show;
 use std::io;
@@ -9,13 +11,45 @@ use std::io::fs::File;
 use std::io::IoResult;
 
 type Err = uint;
+type ParserClosure<'a,I,O> = |I|:'a -> Parser<'a,I,O>;
+//type ParserClosure<'a,I,O> = Fn<I, Parser<'a,I,O>>;
 pub enum Parser<'a,I,O> {
   Done(I,O),
-  Error(I, Err),
-  //Incomplete(|I|:'a -> Parser<I,O>)
-  Incomplete(fn(I) -> Parser<'a,I,O>)
+  Error(Err),
+  Incomplete(ParserClosure<'a,I,O>)
+  //Incomplete(|I|:'a -> Parser<'a,I,O>)
+  //Incomplete(fn(I) -> Parser<'a,I,O>)
 }
 
+impl<'a,I,O> Parser<'a,I,O> {
+  pub fn map<'a,N>(&'a self, f: ParserClosure<'a,&'a O, N>) -> Parser<'a, &'a O, N> {
+    match(self) {
+      &Error(ref e) => Error(*e),
+      &Incomplete(ref cl) => Incomplete(f), //Incomplete(|input:I| { cl(input).map(f) })
+      &Done(_, ref o) => f(o)
+    }
+  }
+
+  pub fn mapf<'a,N>(&'a self, f: |&'a O|:'a -> Option<N>) -> Parser<'a, &'a O, N> {
+    match(self) {
+      &Error(ref e) => Error(*e),
+      &Incomplete(ref cl) => Error(0),//Incomplete(|input: &'a I| {*cl(input).mapf(f)}),
+      &Done(_, ref o) => match f(o) {
+        Some(output) => Done(o, output),
+        None         => Error(0)
+      }
+    }
+  }
+
+  pub fn then<'a,N>(&'a self, f: ParserClosure<'a,&'a I,N>) -> Parser<'a, &'a I, N> {
+    match(self) {
+      &Error(ref e) => Error(*e),
+      &Incomplete(ref cl) => Incomplete(f),
+      &Done(ref i, _) => f(i)
+    }
+
+  }
+}
 /*
 pub fn parse<'a,I,O>(input: I) -> Option<O> {
 
@@ -27,6 +61,7 @@ pub fn parse<'a,I,O>(input: I) -> Option<O> {
   }
 }*/
 
+
 pub fn feed() -> () {
   let v = vec![1, 2, 3, 4, 5];
   let mut acc = v.slice(0, 1);
@@ -34,8 +69,8 @@ pub fn feed() -> () {
   let mut parser = Incomplete(parse);
   loop {
     match parser {
-      Error(i, e) => {
-        println!("error: {} input: {}", e, i);
+      Error(e) => {
+        println!("error: {}", e);
         break;
       },
       Done(i, o)  => {
@@ -54,15 +89,22 @@ pub fn feed() -> () {
 pub fn parse<'a>(input: &'a [u8]) -> Parser<'a, &'a [u8], &'a [u8]> {
 
   match input.len() {
-    0 => Error(input, 0),
+    0 => Error(0),
     1 => Incomplete(parse),
     _ => Done(input.slice_from(2), input.slice(0,2))
   }
 }
 
-pub fn print<'a, T: Show>(input: &'a T) -> Parser<'a, (), ()> {
+pub fn accline<'a>(input: &'a [u8]) -> Parser<'a, &'a [u8], &'a [u8]> {
+  match input.iter().position(|&c| c == '\n' as u8) {
+    None      => Incomplete(accline),
+    Some(pos) => Done(input.slice_from(pos), input.slice(0, pos))
+  }
+}
+
+pub fn print<'a, T: Show>(input: &'a T) -> Parser<'a, &'a T, ()> {
   println!("{}", input);
-  Done((), ())
+  Done(input, ())
 }
 
 pub struct FileProducer {
@@ -85,7 +127,7 @@ impl FileProducer {
         NoProgress => {
           Done((), v)
         },*/
-        _ => Error((), 0)
+        _ => Error(0)
       },
       Ok(i)  => {
         println!("read {} bytes", i);
