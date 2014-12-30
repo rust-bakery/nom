0de679b99281dc9c31ce7cde34c21e27ab000ab9
Geoffroy Couprie <geo.couprie@gmail.com>
2014-12-30 17:43:56 +0100

    parse ini category

--- tests/ini.rs
+++ tests/ini.rs
@@ -10,9 +10,22 @@ use std::str;
 use std::fmt::Show;
 
 fn empty_result(i:&[u8]) -> IResult<&[u8], ()> { Done(i,()) }
+tag!(semicolon ";".as_bytes());
+o!(comment<&[u8], ()> semicolon not_line_ending line_ending ~ empty_result ~);
+tag!(lsb "[".as_bytes());
+tag!(rsb "]".as_bytes());
+fn not_rsb(input:&[u8]) -> IResult<&[u8], &[u8]> {
+  for idx in range(0, input.len()) {
+    if input[idx] == ']' as u8 {
+      return Done(input.slice_from(idx), input.slice(0, idx))
+    }
+  }
+  Done("".as_bytes(), input)
+}
+o!(category<&[u8], &[u8]> lsb ~ not_rsb ~ rsb line_ending);
 
 #[test]
-fn parse_ini_test() {
+fn parse_comment_test() {
   let ini_file = ";comment
 [category]
 parameter=value
@@ -30,9 +43,6 @@ key = value2
 number = 1234
 str = a b cc dd ; comment";
 
-  tag!(semicolon ";".as_bytes());
-  o!(comment<&[u8], ()> semicolon not_line_ending line_ending ~ empty_result ~);
-
   let res = Done((), ini_file.as_bytes()).flat_map(comment);
   println!("{}", res);
   match res {
@@ -41,5 +51,24 @@ str = a b cc dd ; comment";
   }
 
   assert_eq!(res, Done(ini_without_comment.as_bytes(), ()));
+}
+
+#[test]
+fn parse_category_test() {
+  let ini_file = "[category]
+parameter=value
+key = value2";
+
+  let ini_without_category = "parameter=value
+key = value2";
+
+  let res = Done((), ini_file.as_bytes()).flat_map(category);
+  println!("{}", res);
+  match res {
+    IResult::Done(i, o) => println!("i: {} | o: {}", str::from_utf8(i), str::from_utf8(o)),
+    _ => println!("error")
+  }
+
+  assert_eq!(res, Done(ini_without_category.as_bytes(), "category".as_bytes()));
 }
 
