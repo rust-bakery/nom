5651b5fbba5229a2ccc6eee60b452e183391e603
Geoffroy Couprie <geo.couprie@gmail.com>
2015-02-14 18:26:58 +0100

    add address number

--- tests/mp4.rs
+++ tests/mp4.rs
@@ -20,7 +20,17 @@ static CHARS: &'static[u8] = b"0123456789abcdef";
 impl HexDisplay for [u8] {
     fn to_hex(&self, chunk_size: usize) -> String {
       let mut v = Vec::with_capacity(self.len() * 3);
+      let mut i = 0;
       for chunk in self.chunks(chunk_size) {
+        let s = format!("{:08x}", i);
+        let s2: &str = &s;
+        for &ch in s.as_bytes().iter() {
+          v.push(ch);
+        }
+        v.push('\t' as u8);
+
+        i = i + chunk_size;
+
         for &byte in chunk {
           v.push(CHARS[(byte >> 4) as usize]);
           v.push(CHARS[(byte & 0xf) as usize]);
