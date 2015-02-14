ee8eed987e5e4556e5e4f5325560fe1321defef6
Geoffroy Couprie <geo.couprie@gmail.com>
2015-02-14 14:49:37 +0100

    begin implementation of mp4 parser

---
+++ tests/mp4.rs
@@ -0,0 +1,82 @@
+#[macro_use]
+extern crate nom;
+//extern crate serialize;
+
+use nom::{IResult,FlatMapper,Mapper,Producer,ProducerState,FileProducer};
+use nom::IResult::*;
+
+use std::str;
+use std::collections::HashMap;
+//use serialize::hex::ToHex;
+
+pub trait HexDisplay {
+      /// Converts the value of `self` to a hex value, returning the owned
+      ///     /// string.
+  fn to_hex(&self, chunk_size: usize) -> String;
+}
+
+static CHARS: &'static[u8] = b"0123456789abcdef";
+
+impl HexDisplay for [u8] {
+    fn to_hex(&self, chunk_size: usize) -> String {
+      let mut v = Vec::with_capacity(self.len() * 3);
+      for chunk in self.chunks(chunk_size) {
+        for &byte in chunk {
+          v.push(CHARS[(byte >> 4) as usize]);
+          v.push(CHARS[(byte & 0xf) as usize]);
+          v.push(' ' as u8);
+        }
+        if chunk_size > chunk.len() {
+          for i in 0..(chunk_size - chunk.len()) {
+            v.push(' ' as u8);
+            v.push(' ' as u8);
+            v.push(' ' as u8);
+          }
+        }
+        v.push('\t' as u8);
+
+        for &byte in chunk {
+          if (byte >=32 && byte <= 126) || byte >= 128 {
+            v.push(byte);
+          } else {
+            v.push('.' as u8);
+          }
+        }
+        v.push('\n' as u8);
+      }
+
+      unsafe {
+        String::from_utf8_unchecked(v)
+      }
+    }
+}
+
+fn parse_mp4_file(filename: &str) {
+  FileProducer::new(filename, 100).map(|producer: FileProducer| {
+    let mut p = producer;
+    match p.produce() {
+      ProducerState::Data(bytes) => println!("bytes:\n{}", bytes.to_hex(8)),
+      _                          => println!("got error")
+    }
+    /*//p.push(|par| {println!("parsed file: {}", par); par});
+    //p.push(|par| par.flat_map(print));
+    fn pr<'a,'b,'c>(data: &[u8]) -> IResult<'b,&[u8], &[u8]> {
+      println!("bytes: {:?}", &data[0..20]);
+      //Done("".as_bytes(), data).map_res(str::from_utf8);
+      Done("".as_bytes(),"".as_bytes())
+    }
+    pusher!(ps, pr);
+    ps(&mut p);
+    */
+    assert!(false);
+  });
+
+}
+#[test]
+fn file_test() {
+  parse_mp4_file("small.mp4");
+}
+#[test]
+fn bunny_test() {
+  parse_mp4_file("bigbuckbunny.mp4");
+}
