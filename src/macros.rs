0f6f94217745dc0e6fd7d95d2d2f83441b82a8d9
Geoffroy Couprie <geo.couprie@gmail.com>
2015-03-10 09:31:43 +0100

    move the macros tests to the macros file

--- src/macros.rs
+++ src/macros.rs
@@ -649,4 +649,203 @@ macro_rules! take_until_either_and_leave(
   )
 );
 
+#[cfg(test)]
+mod tests {
+  use super::*;
+  use map::*;
+  use internal::Needed;
+  use internal::IResult;
+  use internal::IResult::*;
 
+  #[test]
+  fn is_a() {
+    is_a!(a_or_b   b"ab");
+
+    let a = b"abcd";
+    assert_eq!(a_or_b(a), Done(b"cd", b"ab"));
+
+    let b = b"bcde";
+    assert_eq!(a_or_b(b), Done(b"cde", b"b"));
+
+    let c = b"cdef";
+    assert_eq!(a_or_b(c), Done(b"cdef", b""));
+
+    let d = b"bacdef";
+    assert_eq!(a_or_b(d), Done(b"cdef", b"ba"));
+  }
+
+  #[derive(PartialEq,Eq,Debug)]
+  struct B {
+    a: u8,
+    b: u8
+  }
+
+  #[test]
+  fn chain_and_ignore() {
+    tag!(x "abcd");
+    tag!(y "efgh");
+    fn ret_int(i:&[u8]) -> IResult<&[u8], u8> { Done(i,1) };
+    //o!(z<&[u8], int>  x S x S retInt Z y);
+    o!(z<&[u8], u8>  x ~ x ~ [ ret_int ] ~ y);
+
+    let r = z(b"abcdabcdefgh");
+    assert_eq!(r, Done(b"", 1));
+  }
+
+
+  #[test]
+  fn chain() {
+    tag!(x "abcd");
+    fn temp_ret_int1(i:&[u8]) -> IResult<&[u8], u8> { Done(i,1) };
+    o!(ret_int1<&[u8],u8> x ~ [ temp_ret_int1 ]);
+    fn ret_int2(i:&[u8]) -> IResult<&[u8], u8> { Done(i,2) };
+    chain!(f<&[u8],B>,
+      aa: ret_int1 ~
+      bb: ret_int2 ,
+      ||{B{a: aa, b: bb}}
+    );
+
+    let r = f(b"abcde");
+    assert_eq!(r, Done(b"e", B{a: 1, b: 2}));
+  }
+
+  #[test]
+  fn chain2() {
+    tag!(x "abcd");
+    tag!(y "efgh");
+    //fn temp_ret_int1(i:&[u8]) -> IResult<&[u8], u8> { Done(i,1) };
+    //o!(ret_int1<&[u8],u8> x ~ [ temp_ret_int1 ]);
+    fn ret_int1(i:&[u8]) -> IResult<&[u8], u8> { Done(i,1) };
+    fn ret_int2(i:&[u8]) -> IResult<&[u8], u8> { Done(i,2) };
+    chain!(f<&[u8],B>,
+      x            ~
+      x?           ~
+      aa: ret_int1 ~
+      y            ~
+      bb: ret_int2 ~
+      y            ,
+      ||{B{a: aa, b: bb}});
+
+    let r = f(b"abcdabcdefghefghX");
+    assert_eq!(r, Done(b"X", B{a: 1, b: 2}));
+
+    let r2 = f(b"abcdefghefghX");
+    assert_eq!(r2, Done(b"X", B{a: 1, b: 2}));
+  }
+
+  #[derive(PartialEq,Eq,Debug)]
+  struct C {
+    a: u8,
+    b: Option<u8>
+  }
+
+
+  #[test]
+  fn chain_opt() {
+    tag!(x "abcd");
+    tag!(y "efgh");
+    fn ret_int1(i:&[u8]) -> IResult<&[u8], u8> { Done(i,1) };
+    fn ret_y(i:&[u8]) -> IResult<&[u8], u8> {
+      y(i).map(|_| 2)
+    };
+
+    chain!(f<&[u8],C>,
+      x            ~
+      aa: ret_int1 ~
+      bb: ret_y?   ,
+      ||{C{a: aa, b: bb}});
+
+    let r = f(b"abcdefghX");
+    assert_eq!(r, Done(b"X", C{a: 1, b: Some(2)}));
+
+    let r2 = f(b"abcdWXYZ");
+    assert_eq!(r2, Done(b"WXYZ", C{a: 1, b: None}));
+
+    let r3 = f(b"abcdX");
+    assert_eq!(r3, Incomplete(Needed::Size(4)));
+  }
+
+  #[test]
+  fn alt() {
+    fn work(input: &[u8]) -> IResult<&[u8],&[u8]> {
+      Done(b"", input)
+    }
+
+    #[allow(unused_variables)]
+    fn dont_work(input: &[u8]) -> IResult<&[u8],&[u8]> {
+      Error(3)
+    }
+
+    fn work2(input: &[u8]) -> IResult<&[u8],&[u8]> {
+      Done(input, b"")
+    }
+
+    alt!(alt1<&[u8],&[u8]>, dont_work | dont_work);
+    alt!(alt2<&[u8],&[u8]>, dont_work | work);
+    alt!(alt3<&[u8],&[u8]>, dont_work | dont_work | work2 | dont_work);
+
+    let a = b"abcd";
+    assert_eq!(alt1(a), Error(1));
+    assert_eq!(alt2(a), Done(b"", a));
+    assert_eq!(alt3(a), Done(a, b""));
+  }
+
+  #[test]
+  fn opt() {
+    tag!(x "abcd");
+    opt!(o<&[u8],&[u8]> x);
+
+    let a = b"abcdef";
+    let b = b"bcdefg";
+    assert_eq!(o(a), Done(b"ef", Some(b"abcd")));
+    assert_eq!(o(b), Done(b"bcdefg", None));
+  }
+
+
+  #[test]
+  fn many0() {
+    tag!(x "abcd");
+    many0!(multi<&[u8],&[u8]> x);
+
+    let a = b"abcdef";
+    let b = b"abcdabcdef";
+    let c = b"azerty";
+
+    let res1 = vec![b"abcd"];
+    assert_eq!(multi(a), Done(b"ef", res1));
+    let res2 = vec![b"abcd", b"abcd"];
+    assert_eq!(multi(b), Done(b"ef", res2));
+    assert_eq!(multi(c), Done(b"azerty", Vec::new()));
+  }
+
+  #[test]
+  fn many1() {
+    tag!(x "abcd");
+    many1!(multi<&[u8],&[u8]> x);
+
+    let a = b"abcdef";
+    let b = b"abcdabcdef";
+    let c = b"azerty";
+    let res1 = vec![b"abcd"];
+    assert_eq!(multi(a), Done(b"ef", res1));
+    let res2 = vec![b"abcd", b"abcd"];
+    assert_eq!(multi(b), Done(b"ef", res2));
+    assert_eq!(multi(c), Error(0));
+  }
+
+  #[test]
+  fn take_until_test() {
+    take_until!(x "efgh");
+    let r = x(b"abcdabcdefghijkl");
+    assert_eq!(r, Done(b"ijkl", b"abcdabcd"));
+
+    println!("Done 1\n");
+
+    let r2 = x(b"abcdabcdefgh");
+    assert_eq!(r2, Done(b"", b"abcdabcd"));
+
+    println!("Done 2\n");
+    let r3 = x(b"abcefg");
+    assert_eq!(r3, Incomplete(Needed::Size(7)));
+  }
+}
