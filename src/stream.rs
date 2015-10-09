afd337a5ce185d68003f55a86024de67cad3db73
Geoffroy Couprie <geo.couprie@gmail.com>
2015-10-09 19:59:41 +0200

    Fix file producer compilation
    The PrintLineConsumer seems to work.
    Seeking is not ok yet

--- src/stream.rs
+++ src/stream.rs
@@ -3,10 +3,11 @@
 /// * data lifetimes makes sure that the result of a function applied to a producer cannot live longer than the producer's data (unless there is cloning)
 /// * previous implementation of Producer and Consumer spent its time copying buffers
 /// * the old Consumer was handling everything and buffering data. The new design has the producer handle data, but the consumer makes seeking decision
-use std::io::{self,Read,SeekFrom};
+use std::io::{self,Read,Seek,SeekFrom};
 use std::fs::File;
 use std::path::Path;
 use std::ptr;
+use std::iter::repeat;
 
 //pub type Computation<I,O,S,E> = Box<Fn(S, Input<I>) -> (I,Consumer<I,O,S,E>)>;
 
@@ -53,14 +54,14 @@ pub trait Consumer<I,O,E,M> {
 /// it handles buffer copying and reallocation, to provide streaming patterns.
 /// it depends on the input type I, and the message type M.
 /// the consumer can change the way data is produced (for example, to seek in the source) by sending a message of type M.
-pub trait Producer<I,M> {
+pub trait Producer<'b,I,M> {
   /// Applies a consumer once on the produced data, and return the consumer's state
   ///
   /// a new producer has to implement this method
-  fn apply<'a, O,E>(& mut self, consumer: &'a mut Consumer<I,O,E,M>) -> &'a ConsumerState<O,E,M>;
+  fn apply<'a, O,E>(&'b mut self, consumer: &'a mut Consumer<I,O,E,M>) -> &'a ConsumerState<O,E,M>;
 
   /// Applies a consumer once on the produced data, and returns the generated value if there is one
-  fn run<'a,O,E>(&'a mut self, consumer: &'a mut Consumer<I,O,E,M>)   -> Option<&O> {
+  fn run<'a: 'b,O,E>(&'b mut self, consumer: &'a mut Consumer<I,O,E,M>)   -> Option<&O> {
     if let &ConsumerState::Done(ref o) = self.apply(consumer) {
       Some(o)
     } else {
@@ -75,8 +76,8 @@ pub struct ProducerRepeat<I:Copy> {
   value: I
 }
 
-impl<I:Copy,M> Producer<I,M> for ProducerRepeat<I> {
-  fn apply<'a,O,E>(& mut self, consumer: &'a mut Consumer<I,O,E,M>) -> &'a ConsumerState<O,E,M> {
+impl<'b,I:Copy,M> Producer<'b,I,M> for ProducerRepeat<I> {
+  fn apply<'a,O,E>(&'b mut self, consumer: &'a mut Consumer<I,O,E,M>) -> &'a ConsumerState<O,E,M> {
     if {
       if let &ConsumerState::Continue(_) = consumer.state() {
         true
@@ -122,8 +123,8 @@ pub enum Move {
   Seek(SeekFrom)
 }
 
-impl<'x> Producer<&'x[u8],Move> for MemProducer<'x> {
-  fn apply<'a,O,E>(& mut self, consumer: &'a mut Consumer<&'x[u8],O,E,Move>) -> &'a ConsumerState<O,E,Move> {
+impl<'x,'b> Producer<'b,&'x[u8],Move> for MemProducer<'x> {
+  fn apply<'a,O,E>(&'b mut self, consumer: &'a mut Consumer<&'x[u8],O,E,Move>) -> &'a ConsumerState<O,E,Move> {
     if {
       if let &ConsumerState::Continue(ref m) = consumer.state() {
         match *m {
@@ -213,9 +214,10 @@ pub struct FileProducer {//<'x> {
 impl FileProducer {
 //impl<'x> FileProducer<'x> {
   pub fn new(filename: &str, buffer_size: usize) -> io::Result<FileProducer> {
-    File::open(&Path::new(filename)).map(|f| {
+    File::open(&Path::new(filename)).map(|mut f| {
+      f.seek(SeekFrom::Start(0));
       let mut v = Vec::with_capacity(buffer_size);
-      //v.extend(repeat(0).take(buffer_size));
+      v.extend(repeat(0).take(buffer_size));
       FileProducer {size: buffer_size, file: f, v: v, start: 0, end: 0, state: FileProducerState::Normal }//, ph: PhantomData }
     })
   }
@@ -231,6 +233,7 @@ impl FileProducer {
       },
       Ok(n)  => {
         //println!("read: {} bytes\ndata:\n{}", n, (&self.v).to_hex(8));
+        println!("read: {} bytes\ndata:\n{:?}", n, &self.v);
         /*if n == 0 { 
           Eof(&self.v[..n])
         } else {
@@ -245,78 +248,6 @@ impl FileProducer {
     }
   }
 
-  fn my_apply<'a,'x,O,E>(&'x mut self, consumer: &'a mut Consumer<&'x[u8],O,E,Move>) -> &'a ConsumerState<O,E,Move> {
-    if {
-      if let &ConsumerState::Continue(ref m) = consumer.state() {
-        match *m {
-          Move::Consume(s) => {
-            if self.end - self.start > s {
-              self.start += s
-            } else {
-              panic!("cannot consume past the end of the buffer");
-            }
-          },
-          // FIXME: must seek in the file
-          Move::Seek(SeekFrom::Start(position)) => {
-            if position as usize > self.end {
-              self.start = self.end
-            } else {
-              self.start = position as usize
-            }
-          },
-          // FIXME: must seek in the file
-          Move::Seek(SeekFrom::Current(offset)) => {
-            let next = if offset >= 0 {
-              (self.start as u64).checked_add(offset as u64)
-            } else {
-              (self.start as u64).checked_sub(-offset as u64)
-            };
-            match next {
-              None    => None,
-              Some(u) => {
-                if u as usize > self.end {
-                  self.start = self.end
-                } else {
-                  self.start = u as usize
-                }
-                Some(self.start as u64)
-              }
-            };
-          },
-          // FIXME: must seek in the file
-          Move::Seek(SeekFrom::End(i)) => {
-            let next = if i < 0 {
-              (self.end as u64).checked_sub(-i as u64)
-            } else {
-              // std::io::SeekFrom documentation explicitly allows
-              // seeking beyond the end of the stream, so we seek
-              // to the end of the content if the offset is 0 or
-              // greater.
-              Some(self.end as u64)
-            };
-            match next {
-              // std::io:SeekFrom documentation states that it `is an
-              // error to seek before byte 0.' So it's the sensible
-              // thing to refuse to seek on underflow.
-              None => None,
-              Some(u) => {
-                self.start = u as usize;
-                Some(u)
-              }
-            };
-          }
-        }
-        true
-      } else {
-        false
-      }
-    }
-    {
-      consumer.handle(Input::Element(&self.v[self.start..self.end]))
-    } else {
-      consumer.state()
-    }
-  }
 }
 
 pub fn shift(s: &mut[u8], start: usize, end: usize) {
@@ -329,16 +260,18 @@ pub fn shift(s: &mut[u8], start: usize, end: usize) {
 }
 
 
-impl<'x> Producer<&'x [u8],Move> for FileProducer {
+impl<'x> Producer<'x,&'x [u8],Move> for FileProducer {
 
-  fn apply<'a,O,E>(&mut self, consumer: &'a mut Consumer<&'x[u8],O,E,Move>) -> &'a ConsumerState<O,E,Move> {
+  fn apply<'a,O,E>(&'x mut self, consumer: &'a mut Consumer<&'x[u8],O,E,Move>) -> &'a ConsumerState<O,E,Move> {
+      //consumer.handle(Input::Element(&self.v[self.start..self.end]))
     //self.my_apply(consumer)
     if {
       if let &ConsumerState::Continue(ref m) = consumer.state() {
         match *m {
           Move::Consume(s) => {
-            if self.end - self.start > s {
-              self.start += s
+            println!("start: {}, end: {}, consumed: {}", self.start, self.end, s);
+            if self.end - self.start >= s {
+              self.start = self.start + s;
             } else {
               panic!("cannot consume past the end of the buffer");
             }
@@ -759,6 +692,7 @@ mod tests {
     }
   }
 
+
   #[test]
   fn chain() {
     let mut m = MemProducer::new(&b"abcdefghijklabcdabcd"[..], 8);
@@ -772,6 +706,70 @@ mod tests {
     println!("apply {:?}", m.apply(&mut a));
     println!("apply {:?}", m.apply(&mut a));
     //assert!(false);
+    //
+    //let x = [0, 1, 2, 3, 4];
+    //let b = [1, 2, 3];
+    //assert_eq!(&x[1..3], &b[..]);
+  }
+
+  #[test]
+  fn shift_test() {
+    let mut v = vec![0,1,2,3,4,5];
+    shift(&mut v, 1, 3);
+    assert_eq!(&v[..2], &[1,2][..]);
+    let mut v2 = vec![0,1,2,3,4,5];
+    shift(&mut v2, 2, 6);
+    assert_eq!(&v2[..4], &[2,3,4,5][..]);
+  }
+
+  #[derive(Debug)]
+  struct PrintLineConsumer {
+    state: ConsumerState<(), (), Move>
+  }
+
+  fn lf(i:& u8) -> bool {
+    *i == '\n' as u8
+  }
+  //named!(line<&[u8]>, take_till!(lf));
+  named!(line<&[u8]>, take_until_and_consume!("\n"));
+
+  impl<'a> Consumer<&'a [u8], (), (), Move> for PrintLineConsumer {
+    fn handle(&mut self, input: Input<&'a [u8]>) -> &ConsumerState<(), (), Move> {
+      match input {
+        Input::Empty | Input::Eof(None)           => &self.state,
+        Input::Element(sl) | Input::Eof(Some(sl)) => {
+          //println!("got slice: {:?}", sl);
+          self.state = match line(sl) {
+            IResult::Incomplete(_)  => ConsumerState::Continue(Move::Consume(0)),
+            IResult::Error(_)       => ConsumerState::Error(()),
+            IResult::Done(i,o)      => {
+              println!("found: {}", from_utf8(o).unwrap());
+              //println!("sl: {:?}\ni:{:?}\noffset:{}", sl, i, sl.offset(i));
+              ConsumerState::Continue(Move::Consume(sl.offset(i)))
+            }
+          };
+
+          &self.state
+        }
+      }
+
+    }
+
+    fn state(&self) -> &ConsumerState<(), (), Move> {
+      &self.state
+    }
+  }
+
+  #[test]
+  fn file() {
+    let mut f = FileProducer::new("testfile.txt", 200).unwrap();
+    f.refill(42);
+
+    let mut a  = PrintLineConsumer { state: ConsumerState::Continue(Move::Consume(0)) };
+    for _ in 1..10 {
+      println!("file apply {:?}", f.apply(&mut a));
+    }
+    assert!(false);
   }
 
 }
