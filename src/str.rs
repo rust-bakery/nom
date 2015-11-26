5f27495ccd9e3fb7b40b8742fbea9a976fbe5eb1
Jeroen Bollen <jbinero@gmail.com>
2015-11-26 18:04:29 +0100

    Fixed unit test.

--- src/str.rs
+++ src/str.rs
@@ -10,15 +10,14 @@ use ::{
 };
 
 /// Parses the input for the supplied tag, and returns it on success.
+// TODO: Examples once we have figured out a way to do str-based parsing in Nom.
 pub fn tag_str<'a>(input: &'a str, tag: &str) -> IResult<&'a str, &'a str> {
-    if input.starts_with(tag) {
+    if input.len() < tag.len() {
+        IResult::Incomplete(Needed::Size(input.len()))
+    } else if input.starts_with(tag) {
         IResult::Done(&input[tag.len()..], &input[0..tag.len()])
     } else {
-        if input.len() < tag.len() {
-            IResult::Incomplete(Needed::Size(input.len()))
-        } else {
-            IResult::Error(Err::Position(ErrorKind::Tag, input))
-        }
+        IResult::Error(Err::Position(ErrorKind::Tag, input))
     }
 }
 
@@ -61,7 +60,7 @@ mod test {
     #[test]
     fn tag_str_error() {
         const INPUT: &'static str = "Hello World!";
-        const TAG: &'static str = "Random String";
+        const TAG: &'static str = "Random"; // TAG must be closer than INPUT.
 
         match tag_str(INPUT, TAG) {
             IResult::Error(_) => (),
