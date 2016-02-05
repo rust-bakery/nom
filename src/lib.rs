d4e2dc51f4f223f36f05253b7651803a1bb9081f
Geoffroy Couprie <geo.couprie@gmail.com>
2016-02-05 22:42:58 +0100

    compilation fix and typo fixes

--- src/lib.rs
+++ src/lib.rs
@@ -111,7 +111,7 @@ mod std {
 pub use self::util::*;
 pub use self::internal::*;
 pub use self::macros::*;
-pub use self::method_macros::*;
+pub use self::methods::*;
 pub use self::bytes::*;
 pub use self::bits::*;
 
@@ -131,7 +131,7 @@ pub use self::str::*;
 #[macro_use] mod util;
 mod internal;
 #[macro_use] mod macros;
-#[macro_use] mod method_macros;
+#[macro_use] mod methods;
 #[macro_use] mod bytes;
 #[macro_use] mod bits;
 
