a940a106bb6151ad11ead7c3d81674f03215e554
Pierre Chifflier <chifflier@wzdftpd.net>
2017-03-28 09:48:27 +0200

    Check for overflows in chain! and tuple! when incomplete

--- tests/overflow.rs
+++ tests/overflow.rs
@@ -14,8 +14,32 @@ named!(parser01<&[u8],()>,
     )
 );
 
+// We request a length that would trigger an overflow if computing consumed + requested
+named!(parser02<&[u8],()>,
+    chain!(
+        hdr: take!(1) ~
+        data: take!(18446744073709551615),
+        || ()
+    )
+);
+
+// We request a length that would trigger an overflow if computing consumed + requested
+named!(parser03<&[u8],(&[u8],&[u8])>,
+    tuple!(take!(1),take!(18446744073709551615))
+);
+
 #[test]
-fn overflow_incomplete_test() {
+fn overflow_incomplete_test01() {
   assert_eq!(parser01(&b"3"[..]), IResult::Incomplete(Needed::Unknown));
+}
+
+#[test]
+fn overflow_incomplete_test02() {
+  assert_eq!(parser02(&b"3"[..]), IResult::Incomplete(Needed::Unknown));
+}
+
+#[test]
+fn overflow_incomplete_test03() {
+  assert_eq!(parser03(&b"3"[..]), IResult::Incomplete(Needed::Unknown));
 }
 
