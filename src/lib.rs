7370a0555f998e21c6c4b64e967d66c1c8d29621
Geoffroy Couprie <geo.couprie@gmail.com>
2017-06-17 13:45:19 +0200

    begin a tutorial in crate-level docs

--- src/lib.rs
+++ src/lib.rs
@@ -15,6 +15,124 @@
 //!
 //! See also the [FAQ](http://rust.unhandledexpression.com/nom/FAQ.html).
 //!
+//! # What are parser combinators?
+//!
+//! Parser combinators are a way to build parsers out of small functions. instead of
+//! writing a huge grammar file then generaing code, like you would do with lex and yacc,
+//! you write small functions, to parse small things like a character, or a number,
+//! and then you assemble them in larger and larger functions, that can parse larger
+//! parts of your formats.
+//!
+//! You end up with a list of small functions that you can reuse everywhere you need. Each
+//! of them can be unit tested anf fuzzed separately.
+//!
+//! # nom parser design
+//!
+//! All nom parsers follow the same convention. They are all functions with the following signature:
+//!
+//! ```ignore
+//!  fn parser(input: I) -> IResult<I,O> { ... }
+//! ```
+//!
+//! Here is the definition of that `IResult` type:
+//!
+//! ```
+//! # #[macro_use] extern crate nom;
+//! # use nom::{Err,Needed};
+//! # fn main() {}
+//! pub enum IResult<I,O,E=u32> {
+//!   Done(I,O),
+//!   Error(Err<E>), // indicates the parser encountered an error. E is a custom error type you can redefine
+//!   /// Incomplete contains a Needed, an enum than can represent a known quantity of input data, or unknown
+//!   Incomplete(Needed) // if the parser did not have enough data to decide
+//! }
+//! ```
+//!
+//! What it means:
+//!
+//! * `Done(i,o)` means the parser was successful. `i` is the remaining part of the input, `o` is the correctly parsed value
+//! The remaining part can then be used as input for other parsers called in a sequence
+//! * `Error(e)` indicates the parser encountered an error. The `Err<E>` type is an enum of possible parser errors,
+//! that can also contain a custom error that you'd specify, by redefining the `E` error type
+//! * `Incomplete(i)` means the parser did not have enough information to decide, and tells you, if possible,
+//! how much data it needs
+//!
+//! That way, you could write your own parser that recognizes the letter 'a' like this:
+//!
+//! ```
+//! #[macro_use] extern crate nom;
+//! use nom::{IResult,Needed,Err,ErrorKind};
+//! # fn main() {}
+//!
+//! fn a(input: &[u8]) -> IResult<&[u8], char> {
+//!  // if there is not enough data, we return Ìncomplete
+//!  if input.len() == 0 {
+//!    IResult::Incomplete(Needed::Size(1))
+//!  } else {
+//!    if input[0] == 'a' as u8 {
+//!      // the first part of the returned value is the remaining slice
+//!      IResult::Done(&input[1..], 'a')
+//!    } else {
+//!      IResult::Error(error_code!(ErrorKind::Custom(42)))
+//!    }
+//!  }
+//! }
+//! ```
+//!
+//! Writing all the parsers manually, like this, is dangerous, despite Rust's safety features. There
+//! are still a lot of mistakes one can make. That's why nom provides a list of macros to help in
+//! developing parsers. As an example, here is a parser that would recognize the phrase
+//! "Hello <someone>" and return the name of the person we hail:
+//!
+//! ```
+//! #[macro_use] extern crate nom;
+//! use nom::alpha;
+//!
+//! named!(hello, preceded!(tag!("Hello "), alpha));
+//! # use nom::IResult;
+//! # fn main() {
+//! #  assert_eq!(hello(b"Hello nom."), IResult::Done(&b"."[..], &b"nom"[..]));
+//! # }
+//! ```
+//!
+//! Let's deconstruct it:
+//!
+//! * `named!` generates a function with the correct type. Without `named` here, we could write the parser
+//! as follows:
+//!
+//! ```
+//! #[macro_use] extern crate nom;
+//! use nom::{alpha,IResult};
+//!
+//! fn hello(input: &[u8]) -> IResult<&[u8], &[u8]> {
+//!   preceded!(input,
+//!     tag!("Hello "), alpha)
+//! }
+//! # fn main() {
+//! #  assert_eq!(hello(b"Hello nom."), IResult::Done(&b"."[..], &b"nom"[..]));
+//! # }
+//! ```
+//!
+//! By default, `named` makes a function that takes `&[u8]` as input type, and returns `&[u8]` as output type.
+//! You can override it like this:
+//!
+//! * `named!(hello<&str>, ...):` would take `&[u8]` as input type, and return `&str` as output type.
+//! * `named!(hello<&str, &str>, ...):` would take `&str` as input type, and return `&str` as output type.
+//!
+//! *Note* : when we don't use `named!`, we must pass the input as first argument of the top
+//! level combinator (see the line `preceded!(input,` in the preceding code example). This is a macro trick
+//! in nom to pass input from one combinator to the next by rewriting the call.
+//!
+//! Next part of the parser: `preceded!(tag!("Hello "), alpha))`. Here, `tag!` is a combinator that recognizes
+//! a specific serie of bytes or characters. `alpha` is a function that recognizes alphabetical characters.
+//! The `preceded!` combinator assembles them in a more complex parser: if both parsers are successful,
+//! it returns the result of the second one (`alpha` is preceded by `tag!`).
+//!
+//! *Note* : combinators can assemble other combinators (macros), or parser functions, as long as they follow
+//! the same interface. Here, `alpha` is a parser function already implemented in nom.
+//!
+//!
+//!
 //! # Example
 //!
 //! ```
