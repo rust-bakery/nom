617057425defd24797c16e2f3b24fba27898e939
Evgeny Kurnevsky <kurnevsky@gmail.com>
2017-07-16 09:44:13 +0300

    Add native endianness.

--- src/nom.rs
+++ src/nom.rs
@@ -499,38 +499,154 @@ pub fn le_i64(i:&[u8]) -> IResult<&[u8], i64> {
   map!(i, le_u64, | x | { x as i64 })
 }
 
+/// Recognizes an unsigned 1 byte integer (equivalent to take!(1)
+#[inline]
+pub fn ne_u8(i: &[u8]) -> IResult<&[u8], u8> {
+  if i.len() < 1 {
+    Incomplete(Needed::Size(1))
+  } else {
+    Done(&i[1..], i[0])
+  }
+}
+
+/// Recognizes native endian unsigned 2 bytes integer
+#[inline]
+pub fn ne_u16(i: &[u8]) -> IResult<&[u8], u16> {
+  if i.len() < 2 {
+    Incomplete(Needed::Size(2))
+  } else {
+    let res = unsafe {
+      transmute([i[0], i[1]])
+    };
+    Done(&i[2..], res)
+  }
+}
+
+/// Recognizes native endian unsigned 4 bytes integer
+#[inline]
+pub fn ne_u32(i: &[u8]) -> IResult<&[u8], u32> {
+  if i.len() < 4 {
+    Incomplete(Needed::Size(4))
+  } else {
+    let res = unsafe {
+      transmute([i[0], i[1], i[2], i[3]])
+    };
+    Done(&i[4..], res)
+  }
+}
+
+/// Recognizes native endian unsigned 8 bytes integer
+#[inline]
+pub fn ne_u64(i: &[u8]) -> IResult<&[u8], u64> {
+  if i.len() < 8 {
+    Incomplete(Needed::Size(8))
+  } else {
+    let res = unsafe {
+      transmute([i[0], i[1], i[2], i[3], i[4], i[5], i[6], i[7]])
+    };
+    Done(&i[8..], res)
+  }
+}
+
+/// Recognizes a signed 1 byte integer (equivalent to take!(1)
+#[inline]
+pub fn ne_i8(i:&[u8]) -> IResult<&[u8], i8> {
+  map!(i, ne_u8, | x | { x as i8 })
+}
+
+/// Recognizes native endian signed 2 bytes integer
+#[inline]
+pub fn ne_i16(i:&[u8]) -> IResult<&[u8], i16> {
+  map!(i, ne_u16, | x | { x as i16 })
+}
+
+/// Recognizes native endian signed 4 bytes integer
+#[inline]
+pub fn ne_i32(i:&[u8]) -> IResult<&[u8], i32> {
+  map!(i, ne_u32, | x | { x as i32 })
+}
+
+/// Recognizes native endian signed 8 bytes integer
+#[inline]
+pub fn ne_i64(i:&[u8]) -> IResult<&[u8], i64> {
+  map!(i, ne_u64, | x | { x as i64 })
+}
+
 /// Configurable endianness
 #[derive(Debug,PartialEq,Eq,Clone,Copy)]
 pub enum Endianness {
   Big,
   Little,
+  Native,
 }
 
-/// if the parameter is nom::Endianness::Big, parse a big endian u16 integer,
-/// otherwise a little endian u16 integer
+/// parse a u16 integer, use little endian if the parameter nom::Endianness is not specified
 #[macro_export]
-macro_rules! u16 ( ($i:expr, $e:expr) => ( {if $crate::Endianness::Big == $e { $crate::be_u16($i) } else { $crate::le_u16($i) } } ););
-/// if the parameter is nom::Endianness::Big, parse a big endian u32 integer,
-/// otherwise a little endian u32 integer
+macro_rules! u16 ( ($i:expr, $e:expr) => ( {
+  if $crate::Endianness::Big == $e {
+    $crate::be_u16($i)
+  } else if $crate::Endianness::Native == $e {
+    $crate::ne_u16($i)
+  } else {
+    $crate::le_u16($i)
+  } 
+} ););
+/// parse a u32 integer, use little endian if the parameter nom::Endianness is not specified
 #[macro_export]
-macro_rules! u32 ( ($i:expr, $e:expr) => ( {if $crate::Endianness::Big == $e { $crate::be_u32($i) } else { $crate::le_u32($i) } } ););
-/// if the parameter is nom::Endianness::Big, parse a big endian u64 integer,
-/// otherwise a little endian u64 integer
+macro_rules! u32 ( ($i:expr, $e:expr) => ( {
+  if $crate::Endianness::Big == $e {
+    $crate::be_u32($i)
+  } else if $crate::Endianness::Native == $e {
+    $crate::ne_u32($i)
+  } else {
+    $crate::le_u32($i)
+  } 
+} ););
+/// parse a u64 integer, use little endian if the parameter nom::Endianness is not specified
 #[macro_export]
-macro_rules! u64 ( ($i:expr, $e:expr) => ( {if $crate::Endianness::Big == $e { $crate::be_u64($i) } else { $crate::le_u64($i) } } ););
+macro_rules! u64 ( ($i:expr, $e:expr) => ( {
+  if $crate::Endianness::Big == $e {
+    $crate::be_u64($i)
+  } else if $crate::Endianness::Native == $e {
+    $crate::ne_u64($i)
+  } else {
+    $crate::le_u64($i)
+  } 
+} ););
 
-/// if the parameter is nom::Endianness::Big, parse a big endian i16 integer,
-/// otherwise a little endian i16 integer
+/// parse a i16 integer, use little endian if the parameter nom::Endianness is not specified
 #[macro_export]
-macro_rules! i16 ( ($i:expr, $e:expr) => ( {if $crate::Endianness::Big == $e { $crate::be_i16($i) } else { $crate::le_i16($i) } } ););
-/// if the parameter is nom::Endianness::Big, parse a big endian i32 integer,
-/// otherwise a little endian i32 integer
+macro_rules! i16 ( ($i:expr, $e:expr) => ( {
+  if $crate::Endianness::Big == $e {
+    $crate::be_i16($i)
+  } else if $crate::Endianness::Native == $e {
+    $crate::ne_i16($i)
+  } else {
+    $crate::le_i16($i)
+  } 
+} ););
+/// parse a i32 integer, use little endian if the parameter nom::Endianness is not specified
 #[macro_export]
-macro_rules! i32 ( ($i:expr, $e:expr) => ( {if $crate::Endianness::Big == $e { $crate::be_i32($i) } else { $crate::le_i32($i) } } ););
-/// if the parameter is nom::Endianness::Big, parse a big endian i64 integer,
-/// otherwise a little endian i64 integer
+macro_rules! i32 ( ($i:expr, $e:expr) => ( {
+  if $crate::Endianness::Big == $e {
+    $crate::be_i32($i)
+  } else if $crate::Endianness::Native == $e {
+    $crate::ne_i32($i)
+  } else {
+    $crate::le_i32($i)
+  } 
+} ););
+/// parse a i64 integer, use little endian if the parameter nom::Endianness is not specified
 #[macro_export]
-macro_rules! i64 ( ($i:expr, $e:expr) => ( {if $crate::Endianness::Big == $e { $crate::be_i64($i) } else { $crate::le_i64($i) } } ););
+macro_rules! i64 ( ($i:expr, $e:expr) => ( {
+  if $crate::Endianness::Big == $e {
+    $crate::be_i64($i)
+  } else if $crate::Endianness::Native == $e {
+    $crate::ne_i64($i)
+  } else {
+    $crate::le_i64($i)
+  }
+} ););
 
 /// Recognizes big endian 4 bytes floating point number
 #[inline]
