9e032bbf82031d13c3ee52f624fa077766e47f26
Geoffroy Couprie <geo.couprie@gmail.com>
2017-12-10 16:02:27 +0100

    f32::from_bits is apparently not available in no_std

--- src/nom.rs
+++ src/nom.rs
@@ -16,6 +16,7 @@ use traits::{need_more, AtEof};
 use std::ops::{Range,RangeFrom,RangeTo};
 use traits::{Compare,CompareResult,Slice};
 use util::ErrorKind;
+use std::mem::transmute;
 
 #[inline]
 pub fn tag_cl<'a,'b>(rec:&'a[u8]) ->  Box<Fn(&'b[u8]) -> IResult<&'b[u8], &'b[u8]> + 'a> {
@@ -542,7 +543,11 @@ macro_rules! i64 ( ($i:expr, $e:expr) => ( {if Endianness::Big == $e { be_i64($i
 pub fn be_f32(input: &[u8]) -> IResult<&[u8], f32> {
   match be_u32(input) {
     Err(e) => Err(e),
-    Ok((i,o)) => Ok((i, f32::from_bits(o)))
+    Ok((i,o)) => {
+      unsafe {
+        Ok((i, transmute::<u32, f32>(o)))
+      }
+    }
   }
 }
 
@@ -551,7 +556,11 @@ pub fn be_f32(input: &[u8]) -> IResult<&[u8], f32> {
 pub fn be_f64(input: &[u8]) -> IResult<&[u8], f64> {
   match be_u64(input) {
     Err(e) => Err(e),
-    Ok((i,o)) => Ok((i, f64::from_bits(o)))
+    Ok((i,o)) => {
+      unsafe {
+        Ok((i, transmute::<u64, f64>(o)))
+      }
+    }
   }
 }
 
@@ -560,7 +569,11 @@ pub fn be_f64(input: &[u8]) -> IResult<&[u8], f64> {
 pub fn le_f32(input: &[u8]) -> IResult<&[u8], f32> {
   match le_u32(input) {
     Err(e) => Err(e),
-    Ok((i,o)) => Ok((i, f32::from_bits(o)))
+    Ok((i,o)) => {
+      unsafe {
+        Ok((i, transmute::<u32, f32>(o)))
+      }
+    }
   }
 }
 
@@ -569,7 +582,11 @@ pub fn le_f32(input: &[u8]) -> IResult<&[u8], f32> {
 pub fn le_f64(input: &[u8]) -> IResult<&[u8], f64> {
   match le_u64(input) {
     Err(e) => Err(e),
-    Ok((i,o)) => Ok((i, f64::from_bits(o)))
+    Ok((i,o)) => {
+      unsafe {
+        Ok((i, transmute::<u64, f64>(o)))
+      }
+    }
   }
 }
 
