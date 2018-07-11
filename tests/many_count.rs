eafd5b2c2204f1391fe3aa17b28146bde85cd33b
name <e@ma.il>
2018-07-12 05:26:25 +0800

    Fix test in many_count.rs

--- tests/many_count.rs
+++ tests/many_count.rs
@@ -29,7 +29,7 @@ fn many1_count_test() {
   assert_eq!(count1_digits(CompleteStr("1,2,3,4,5,6,7,8,9,0,")), Ok((CompleteStr(""), 10)));
   assert_eq!(
     count1_digits(CompleteStr("hello")),
-    Err(nom::Err::Error(nom::simple_errors::Context::Code(
+    Err(nom::Err::Error(nom::Context::Code(
       CompleteStr("hello"),
       nom::ErrorKind::Many1Count
     )))
