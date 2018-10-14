5edc06be35574a93df0865bb7a3e1dc52a534b9c
Geoffroy Couprie <geo.couprie@gmail.com>
2018-10-14 15:08:36 +0200

    fix compilation error
    the unify_type trick is already performed in simple error mode by the
    error_node_position macro, and the verbose-errors design ensures that we
    don't need it here

--- src/macros.rs
+++ src/macros.rs
@@ -542,21 +542,15 @@ macro_rules! add_return_error (
   ($i:expr, $code:expr, $submac:ident!( $($args:tt)* )) => (
     {
       use $crate::lib::std::result::Result::*;
-      use $crate::{Err,Context,ErrorKind};
+      use $crate::{Err,ErrorKind};
 
-      let error_context = Context::Code($i, $code);
-      fn unify_types<I,E>(_: &Context<I,E>, _: &Context<I,E>) {}
       match $submac!($i, $($args)*) {
-        Ok((i, o))    => Ok((i, o)),
-        Err(Err::Error(e))      => {
-          unify_types(&e, &error_context);
-          let Context::Code(i, code) = error_context;
-          Err(Err::Error(error_node_position!(i, code, e)))
+        Ok((i, o)) => Ok((i, o)),
+        Err(Err::Error(e)) => {
+          Err(Err::Error(error_node_position!($i, $code, e)))
         },
-        Err(Err::Failure(e))      => {
-          unify_types(&e, &error_context);
-          let Context::Code(i, code) = error_context;
-          Err(Err::Failure(error_node_position!(i, code, e)))
+        Err(Err::Failure(e)) => {
+          Err(Err::Failure(error_node_position!($i, $code, e)))
         },
         Err(e) => Err(e),
       }
