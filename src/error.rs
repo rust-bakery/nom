56becaa7061eebccd73d51d293a049166df999da
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2019-04-24 14:50:09 +0200

    map_parser: don't error if we don't consume all the input
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- src/error.rs
+++ src/error.rs
@@ -183,7 +183,6 @@ pub enum ErrorKind {
   TooLarge,
   Many0Count,
   Many1Count,
-  MapParser,
 }
 
 #[cfg_attr(rustfmt, rustfmt_skip)]
@@ -255,7 +254,6 @@ pub fn error_to_u32(e: &ErrorKind) -> u32 {
     ErrorKind::TooLarge                  => 71,
     ErrorKind::Many0Count                => 72,
     ErrorKind::Many1Count                => 73,
-    ErrorKind::MapParser                 => 74,
   }
 }
 
@@ -329,7 +327,6 @@ impl ErrorKind {
       ErrorKind::TooLarge                  => "Needed data size is too large",
       ErrorKind::Many0Count                => "Count occurrence of >=0 patterns",
       ErrorKind::Many1Count                => "Count occurrence of >=1 patterns",
-      ErrorKind::MapParser                 => "Map on Parser",
     }
   }
 }
