0139773a61401b2bc792dd0a99c9869275ac6e1c
Geoffroy Couprie <geo.couprie@gmail.com>
2019-05-14 14:46:22 +0200

    port dbg_dmp to functions

--- src/util.rs
+++ src/util.rs
@@ -1,5 +1,9 @@
 #[cfg(feature = "alloc")]
 use lib::std::vec::Vec;
+#[cfg(feature = "std")]
+use internal::IResult;
+#[cfg(feature = "std")]
+use std::fmt::Debug;
 
 #[cfg(feature = "std")]
 /// Helper trait to show a byte slice as a hex dump
@@ -133,6 +137,41 @@ macro_rules! dbg (
       dbg!($i, call!($f));
   );
 );
+
+/// Prints a message and the input if the parser fails
+///
+/// The message prints the `Error` or `Incomplete`
+/// and the parser's calling code.
+///
+/// It also displays the input in hexdump format
+///
+/// ```rust
+/// use nom::{util::dbg_dmp, bytes::complete::tag};
+///
+/// fn f(i: &[u8]) -> IResult<&[u8], &[u8]> {
+///   dbg_dmp(tag("abcd"))(i)
+/// }
+///
+///   let a = &b"efghijkl"[..];
+///
+/// // Will print the following message:
+/// // Error(Position(0, [101, 102, 103, 104, 105, 106, 107, 108])) at l.5 by ' tag ! ( "abcd" ) '
+/// // 00000000        65 66 67 68 69 6a 6b 6c         efghijkl
+/// f(a);
+/// ```
+#[cfg(feature = "std")]
+fn dbg_dmp<'a, F, O, E: Debug>(f: F, context: &'static str) -> impl Fn(&'a [u8]) -> IResult<&'a [u8], O, E>
+  where F: Fn(&'a [u8]) -> IResult<&'a [u8], O, E> {
+  move |i: &'a [u8]| {
+      match f(i) {
+        Err(e) => {
+          println!("{}: Error({:?}) at:\n{}", context, e, i.to_hex(8));
+          Err(e)
+        },
+        a => a,
+      }
+  }
+}
 
 /// Prints a message and the input if the parser fails
 ///
