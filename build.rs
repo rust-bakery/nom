9ef2c6718b5286d97fdca99ca48464a4e6a324a5
Sergio Benitez <sb@sergio.bz>
2019-05-28 11:00:42 -0700

    update version_check to 0.9

--- build.rs
+++ build.rs
@@ -2,12 +2,8 @@ extern crate version_check;
 
 fn main() {
   match version_check::is_min_version("1.28.0") {
-    Some((true, _actual_version)) => {
-      println!("cargo:rustc-cfg=stable_i128");
-    }
-    Some(_) => (),
-    None => {
-      eprintln!("couldn't query version info from rustc");
-    }
+    Some(true) => println!("cargo:rustc-cfg=stable_i128"),
+    Some(false) => (),
+    None => eprintln!("couldn't query version info from rustc"),
   }
 }
