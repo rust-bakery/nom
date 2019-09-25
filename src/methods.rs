851706460a9311f7bbae8e9b7ee497c7188df0a3
Geoffroy Couprie <geo.couprie@gmail.com>
2019-09-25 17:30:57 +0200

    fix compile_error macro usage

--- src/methods.rs
+++ src/methods.rs
@@ -3,18 +3,18 @@
 /// do not use: method combinators moved to the nom-methods crate
 #[macro_export]
 macro_rules! method (
-  ($($args:tt)*) => (compile_error!("method combinators moved to the nom-methods crate"));
+  ($($args:tt)*) => (compile_error!("method combinators moved to the nom-methods crate"););
 );
 
 /// do not use: method combinators moved to the nom-methods crate
 #[macro_export]
 macro_rules! call_m (
-  ($($args:tt)*) => (compile_error!("method combinators moved to the nom-methods crate"));
+  ($($args:tt)*) => (compile_error!("method combinators moved to the nom-methods crate"););
 );
 
 /// do not use: method combinators moved to the nom-methods crate
 #[macro_export]
 macro_rules! apply_m (
-  ($($args:tt)*) => (compile_error!("method combinators moved to the nom-methods crate"));
+  ($($args:tt)*) => (compile_error!("method combinators moved to the nom-methods crate"););
 );
 
