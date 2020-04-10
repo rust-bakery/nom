432559c9176c4698aa2541c13393e38051d00eff
Geoffroy Couprie <contact@geoffroycouprie.com>
2020-04-10 16:17:54 +0200

    add a test for FnMut

--- tests/fnmut.rs
+++ tests/fnmut.rs
@@ -1,11 +1,11 @@
 extern crate nom;
 
 use nom::{
-  error::ErrorKind,
-  number::streaming::le_u64,
-  multi::many0,
   bytes::complete::tag,
-  Err, IResult, Needed,
+  error::ErrorKind,
+  multi::{many0, many0_count},
+  number::streaming::le_u64,
+  Err, IResult, Needed, Parser,
 };
 
 #[test]
@@ -13,14 +13,31 @@ fn parse() {
   let mut counter = 0;
 
   let res = {
-      let mut parser = many0::<_, _, (), _>(|i| {
-          counter += 1;
-          tag("abc")(i)
-      });
+    let mut parser = many0::<_, _, (), _>(|i| {
+      counter += 1;
+      tag("abc")(i)
+    });
 
-      parser("abcabcabcabc").unwrap()
+    parser("abcabcabcabc").unwrap()
   };
 
   println!("res: {:?}", res);
   assert_eq!(counter, 5);
+}
+
+#[test]
+fn accumulate() {
+  let mut v = Vec::new();
+
+  let (i, count) = {
+    let mut parser = many0_count::<_, _, (), _>(|i| {
+      let (i, o) = tag("abc")(i)?;
+      v.push(o);
+      Ok((i, ()))
+    });
+    parser("abcabcabcabc").unwrap()
+  };
+
+  println!("v: {:?}", v);
+  assert_eq!(v.len(), 4);
 }
