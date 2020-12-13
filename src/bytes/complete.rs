69810235dba79d0fac086c8594ac3bb3cabb107f
Tom Alexander <tom@fizz.buzz>
2020-12-13 16:48:12 -0500

    Apply the same change to take_until_parser_matches_and_consume.

--- src/bytes/complete.rs
+++ src/bytes/complete.rs
@@ -573,14 +573,13 @@ pub fn take_until_parser_matches_and_consume<F, Input, O, Error>(
   mut f: F,
 ) -> impl FnMut(Input) -> IResult<Input, Input, Error>
 where
-  Input: InputTake + InputLength + Clone,
+  Input: InputTake + InputIter + InputLength + Clone,
   F: Parser<Input, O, Error>,
   Error: ParseError<Input>,
 {
   move |input: Input| {
     let i = input.clone();
-    let input_length = i.input_len();
-    for ind in 0..=input_length {
+    for (ind, _) in i.iter_indices() {
       let (remaining, taken) = i.take_split(ind);
       match f.parse(remaining) {
         Err(_) => (),
@@ -588,6 +587,17 @@ where
           let res: IResult<Input, Input, Error> = Ok((inner_remaining, taken));
           return res;
         }
+      }
+    }
+    // Attempt to match one last time past the end of the input. This
+    // allows for 0-length combinators to be used (for example, an eof
+    // combinator).
+    let (remaining, taken) = i.take_split(i.input_len());
+    match f.parse(remaining) {
+      Err(_) => (),
+      Ok((inner_remaining, _parser_result)) => {
+        let res: IResult<Input, Input, Error> = Ok((inner_remaining, taken));
+        return res;
       }
     }
     Err(Err::Error(Error::from_error_kind(
