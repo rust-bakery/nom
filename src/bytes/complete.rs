6805b797a776af0a015f19a095b336463affffad
Tom Alexander <tom@fizz.buzz>
2020-12-13 16:26:34 -0500

    Switch take_until_parser_matches to iterating over indices as suggested by @NickNick on github.
    This avoids the issue of splitting an index of a string between unicode codepoints.

--- src/bytes/complete.rs
+++ src/bytes/complete.rs
@@ -506,14 +506,13 @@ pub fn take_until_parser_matches<F, Input, O, Error>(
   mut f: F,
 ) -> impl FnMut(Input) -> IResult<Input, Input, Error>
 where
-  Input: InputTake + InputLength + Clone,
+  Input: InputTake + InputIter + InputLength + Clone,
   F: Parser<Input, O, Error>,
   Error: ParseError<Input>,
 {
   move |input: Input| {
     let i = input.clone();
-    let input_length = i.input_len();
-    for ind in 0..=input_length {
+    for (ind, _) in i.iter_indices() {
       let (remaining, _taken) = i.take_split(ind);
       match f.parse(remaining) {
         Err(_) => (),
@@ -521,6 +520,17 @@ where
           let res: IResult<Input, Input, Error> = Ok(i.take_split(ind));
           return res;
         }
+      }
+    }
+    // Attempt to match one last time past the end of the input. This
+    // allows for 0-length combinators to be used (for example, an eof
+    // combinator).
+    let (remaining, _taken) = i.take_split(i.input_len());
+    match f.parse(remaining) {
+      Err(_) => (),
+      Ok(_) => {
+        let res: IResult<Input, Input, Error> = Ok(i.take_split(i.input_len()));
+        return res;
       }
     }
     Err(Err::Error(Error::from_error_kind(
