016a126671f298eebe0c2744cbae4ca89fdafe70
Nathan West <Lucretiel@gmail.com>
2020-12-20 14:04:30 -0500

    impl Parser for Box<dyn Parser>

--- src/internal.rs
+++ src/internal.rs
@@ -288,6 +288,12 @@ where
   }
 }
 
+impl<'a, I, O, E> Parser<I, O, E> for Box<dyn Parser<I, O, E> + 'a> {
+  fn parse(&mut self, input: I) -> IResult<I, O, E> {
+    (**self).parse(input)
+  }
+}
+
 /// Implementation of `Parser:::map`
 #[allow(missing_doc_code_examples)]
 pub struct Map<F, G, O1> {
