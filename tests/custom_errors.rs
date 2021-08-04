71b98465fe5a4336dd6cdd212960056f3ae8c7c6
Constantin Nickel <constantin.nickel@gmail.com>
2021-08-04 18:14:49 +0200

    remove macros from custom error tests

--- tests/custom_errors.rs
+++ tests/custom_errors.rs
@@ -1,14 +1,13 @@
 #![allow(dead_code)]
-#![cfg_attr(feature = "cargo-clippy", allow(block_in_if_condition_stmt))]
 
-#[macro_use]
-extern crate nom;
-
+use nom::bytes::streaming::tag;
 use nom::character::streaming::digit1 as digit;
+use nom::combinator::verify;
 use nom::error::{ErrorKind, ParseError};
+#[cfg(feature = "alloc")]
+use nom::multi::count;
+use nom::sequence::terminated;
 use nom::IResult;
-
-use std::convert::From;
 
 #[derive(Debug)]
 pub struct CustomError(String);
@@ -31,19 +30,19 @@ impl<'a> ParseError<&'a str> for CustomError {
 
 fn test1(input: &str) -> IResult<&str, &str, CustomError> {
   //fix_error!(input, CustomError, tag!("abcd"))
-  tag!(input, "abcd")
+  tag("abcd")(input)
 }
 
 fn test2(input: &str) -> IResult<&str, &str, CustomError> {
   //terminated!(input, test1, fix_error!(CustomError, digit))
-  terminated!(input, test1, digit)
+  terminated(test1, digit)(input)
 }
 
 fn test3(input: &str) -> IResult<&str, &str, CustomError> {
-  verify!(input, test1, |s: &str| { s.starts_with("abcd") })
+  verify(test1, |s: &str| s.starts_with("abcd"))(input)
 }
 
 #[cfg(feature = "alloc")]
 fn test4(input: &str) -> IResult<&str, Vec<&str>, CustomError> {
-  count!(input, test1, 4)
+  count(test1, 4)(input)
 }
