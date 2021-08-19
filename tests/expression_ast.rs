ea761e61c482f94e7209bb08aa976c7a0278e6b6
cenodis <57576911+cenodis@users.noreply.github.com>
2021-08-19 10:21:57 +0200

    Implement ternary operator in expression_ast

--- tests/expression_ast.rs
+++ tests/expression_ast.rs
@@ -10,7 +10,7 @@ use nom::{
 };
 
 // Elements of the abstract syntax tree (ast) that represents an expression.
-#[derive(Debug, Eq, PartialEq)]
+#[derive(Debug)]
 pub enum Expr {
   // A number literal.
   Number(i64),
@@ -23,6 +23,8 @@ pub enum Expr {
   Div(Box<Expr>, Box<Expr>),
   // The function call operation. Left is the expression the function is called on, right is the list of parameters.
   Call(Box<Expr>, Vec<Expr>),
+  // The ternary operator, the expressions from left to right are: The condition, the true case, the false case.
+  Ternary(Box<Expr>, Box<Expr>, Box<Expr>),
 }
 
 // We need to be able to encode different types of operators. So we make the operator parsers output one of the Operator variants.
@@ -32,11 +34,12 @@ enum Operator<'a> {
   // The function call operator. In addition to its own representation "()" it carries additional information that we need to keep here.
   // Specifically the vector of expressions that make up the parameters.
   Call(Vec<Expr>),
+  // The ternary operator can contain a single expression.
+  Ternary(Expr),
 }
 
 // Convenience parser for raw operators.
-fn token<'a>(t: &'a str) -> impl FnMut(&'a str) -> IResult<&'a str, Operator<'a>>
-{
+fn token<'a>(t: &'a str) -> impl FnMut(&'a str) -> IResult<&'a str, Operator<'a>> {
   move |input: &'a str| {
     map(tag(t), |s: &str| Operator::Raw(s))(input)
   }
@@ -55,6 +58,27 @@ fn function_call(i: &str) -> IResult<&str, Operator> {
   )(i)
 }
 
+// The ternary operator is actually just a binary operator that contains another expression. So it can be
+// handled similarly to the function call operator except its in a binary position and can only contain
+// a single expression.
+// 
+// For example the expression `a<b ? a : b` is handled similarly to the function call operator, the
+// ? is treated like an opening bracket and the : is treated like a closing bracket.
+//
+// For the outer expression the result looks like `a<b ?: b`. Where `?:` is a single operator. The
+// subexpression is contained within the operator in the same way that the function call operator
+// contains subexpressions.
+fn ternary_operator(i: &str) -> IResult<&str, Operator> {
+  map(
+    delimited(
+      tag("?"), 
+      expression, 
+      tag(":")
+    ),
+    |e: Expr| Operator::Ternary(e)
+  )(i)
+}
+
 // The actual expression parser .
 fn expression(i: &str) -> IResult<&str, Expr> {
   precedence(
@@ -69,7 +93,9 @@ fn expression(i: &str) -> IResult<&str, Expr> {
       binary_op(4, Assoc::Left, alt((
         token("+"),
         token("-"),
-      )))
+      ))),
+      // Ternary operators are just binary operators with a subexpression.
+      binary_op(5, Assoc::Right, ternary_operator),
     )),
     alt((
       map_res(digit,
@@ -87,13 +113,20 @@ fn expression(i: &str) -> IResult<&str, Expr> {
       match op {
         // Unary minus gets evaluated to the same representation as a multiplication with -1.
         Prefix(Raw("-"), e) => Ok(Expr::Mul(Expr::Number(-1).into(), e.into())),
+        
         // The list of parameters are taken from the operator and placed into the ast.
         Postfix(e, Call(p)) => Ok(Expr::Call(e.into(), p)),
+        
+        // Meaning is assigned to the expressions of the ternary operator during evaluation.
+        // The lhs becomes the condition, the contained expression is the true case, rhs the false case.
+        Binary(lhs, Ternary(e), rhs) => Ok(Expr::Ternary(lhs.into(), e.into(), rhs.into())),
+        
         // Raw operators get turned into their respective ast nodes.
         Binary(lhs, Raw("*"), rhs) => Ok(Expr::Mul(lhs.into(), rhs.into())),
         Binary(lhs, Raw("/"), rhs) => Ok(Expr::Div(lhs.into(), rhs.into())),
         Binary(lhs, Raw("+"), rhs) => Ok(Expr::Add(lhs.into(), rhs.into())),
         Binary(lhs, Raw("-"), rhs) => Ok(Expr::Sub(lhs.into(), rhs.into())),
+        
         _ => Err("Invalid op combo."),
       }
     }
@@ -106,4 +139,9 @@ fn expression_test() {
     expression("-2*max(2,3)-2").map(|(i, x)| (i, format!("{:?}", x))),
     Ok(("", String::from("Sub(Mul(Mul(Number(-1), Number(2)), Call(Identifier(\"max\"), [Number(2), Number(3)])), Number(2))")))
   );
+  
+ assert_eq!(
+   expression("a?2+c:-2*2").map(|(i, x)| (i, format!("{:?}", x))),
+   Ok(("", String::from("Ternary(Identifier(\"a\"), Add(Number(2), Identifier(\"c\")), Mul(Mul(Number(-1), Number(2)), Number(2)))")))
+ );
 }
