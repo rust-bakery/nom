d4784ed024478f86b4696cd727a0b5bd09f27af5
link2xt <link2xt@testrun.org>
2021-09-28 12:06:59 +0000

    Add BitsParseError

--- src/bits/mod.rs
+++ src/bits/mod.rs
@@ -12,6 +12,36 @@ use crate::internal::{Err, IResult, Needed};
 use crate::lib::std::ops::RangeFrom;
 use crate::traits::{Slice, ErrorConvert};
 
+/// An error type for bits parser that can hold information
+/// abount the bit offset in addition to other error information.
+pub struct BitsParseError<E> {
+    inner: E,
+    offset: usize
+}
+
+impl<E> From<E> for BitsParseError<E> {
+    fn from(inner: E) -> Self {
+        Self {
+            inner,
+            offset: 0
+        }
+    }
+}
+
+impl<E> BitsParseError<E> {
+    /// Creates a new BitsParseError from any other error and offset
+    fn new(inner: E, offset: usize) -> Self {
+        Self {
+            inner,
+            offset
+        }
+    }
+
+    /// Convert BitsParseError into the underlying error type.
+    pub fn into_inner(self) -> E {
+        self.inner
+    }
+}
 
 /// Converts a byte-level input to a bit-level input, for consumption by a parser that uses bits.
 ///
@@ -34,27 +64,27 @@ use crate::traits::{Slice, ErrorConvert};
 ///
 /// assert_eq!(take_4_bits( sl ), Ok( (&sl[1..], 0xA) ));
 /// ```
-pub fn bits<I, O, E1: ParseError<(I, usize)>+ErrorConvert<E2>, E2: ParseError<I>, P>(parser: P) -> impl Fn(I) -> IResult<I, O, E2>
+pub fn bits<I, O, E: ParseError<I>, P>(parser: P) -> impl Fn(I) -> IResult<I, O, E>
 where
   I: Slice<RangeFrom<usize>>,
-  P: Fn((I, usize)) -> IResult<(I, usize), O, E1>,
+  P: Fn((I, usize)) -> IResult<(I, usize), O, BitsParseError<E>>,
 {
   move |input: I| match parser((input, 0)) {
     Ok(((rest, offset), res)) => {
       let byte_index = offset / 8 + if offset % 8 == 0 { 0 } else { 1 };
-      Ok((rest.slice(byte_index..), res))
+      Ok((rest.slice(byte_index..), res.into()))
     }
     Err(Err::Incomplete(n)) => Err(Err::Incomplete(n.map(|u| u / 8 + 1))),
-    Err(Err::Error(e)) => Err(Err::Error(e.convert())),
-    Err(Err::Failure(e)) => Err(Err::Failure(e.convert())),
+    Err(Err::Error(e)) => Err(Err::Error(e.into_inner())),
+    Err(Err::Failure(e)) => Err(Err::Failure(e.into_inner())),
   }
 }
 
 #[doc(hidden)]
-pub fn bitsc<I, O, E1: ParseError<(I, usize)>+ErrorConvert<E2>, E2: ParseError<I>, P>(input: I, parser: P) -> IResult<I, O, E2>
+pub fn bitsc<I, O, E: ParseError<I>, P>(input: I, parser: P) -> IResult<I, O, E>
 where
   I: Slice<RangeFrom<usize>>,
-  P: Fn((I, usize)) -> IResult<(I, usize), O, E1>,
+  P: Fn((I, usize)) -> IResult<(I, usize), O, BitsParseError<E>>,
 {
   bits(parser)(input)
 }
@@ -84,10 +114,10 @@ where
 ///
 /// assert_eq!(parse( input ), Ok(( &[][..], (0xd, 0xea, &[0xbe, 0xaf][..]) )));
 /// ```
-pub fn bytes<I, O, E1: ParseError<I>+ErrorConvert<E2>, E2: ParseError<(I, usize)>, P>(parser: P) -> impl Fn((I, usize)) -> IResult<(I, usize), O, E2>
+pub fn bytes<I, O, E: ParseError<I>, P>(parser: P) -> impl Fn((I, usize)) -> IResult<(I, usize), O, BitsParseError<E>>
 where
   I: Slice<RangeFrom<usize>> + Clone,
-  P: Fn(I) -> IResult<I, O, E1>,
+  P: Fn(I) -> IResult<I, O, E>,
 {
   move |(input, offset): (I, usize)| {
     let inner = if offset % 8 != 0 {
@@ -95,25 +125,24 @@ where
     } else {
       input.slice((offset / 8)..)
     };
-    let i = (input.clone(), offset);
     match parser(inner) {
       Ok((rest, res)) => Ok(((rest, 0), res)),
       Err(Err::Incomplete(Needed::Unknown)) => Err(Err::Incomplete(Needed::Unknown)),
       Err(Err::Incomplete(Needed::Size(sz))) => Err(match sz.checked_mul(8) {
         Some(v) => Err::Incomplete(Needed::Size(v)),
-        None => Err::Failure(E2::from_error_kind(i, ErrorKind::TooLarge)),
+        None => Err::Failure(BitsParseError::new(E::from_error_kind(input.clone(), ErrorKind::TooLarge), offset)),
       }),
-      Err(Err::Error(e)) => Err(Err::Error(e.convert())),
-      Err(Err::Failure(e)) => Err(Err::Failure(e.convert())),
+      Err(Err::Error(e)) => Err(Err::Error(e.into())),
+      Err(Err::Failure(e)) => Err(Err::Failure(e.into())),
     }
   }
 }
 
 #[doc(hidden)]
-pub fn bytesc<I, O, E1: ParseError<I>+ErrorConvert<E2>, E2: ParseError<(I, usize)>, P>(input: (I, usize), parser: P) -> IResult<(I, usize), O, E2>
+pub fn bytesc<I, O, E: ParseError<I>, P>(input: (I, usize), parser: P) -> IResult<(I, usize), O, BitsParseError<E>>
 where
   I: Slice<RangeFrom<usize>> + Clone,
-  P: Fn(I) -> IResult<I, O, E1>,
+  P: Fn(I) -> IResult<I, O, E>,
 {
   bytes(parser)(input)
 }
