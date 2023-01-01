fe01bf6d42df51d5971142bae1c27432ae5f43c6
Mustafa Guler <mguler96@gmail.com>
2023-01-01 18:06:48 -0500

    loosen trait bounds for nom::multi::fill (#1524)
    Previously the parser provided to fill required a Fn bound, which causes
    it to fail when used with many other parsers that return an impl FnMut.
    This also brings it more in line with other combinators in the multi
    module.
    
    Co-authored-by: Geoffroy Couprie <contact@geoffroycouprie.com>

--- tests/issues.rs
+++ tests/issues.rs
@@ -217,6 +217,32 @@ fn issue_1282_findtoken_char() {
 }
 
 #[test]
+fn issue_x_looser_fill_bounds() {
+  use nom::{
+    bytes::streaming::tag, character::streaming::digit1, error_position, multi::fill,
+    sequence::terminated,
+  };
+
+  fn fill_pair(i: &[u8]) -> IResult<&[u8], [&[u8]; 2]> {
+    let mut buf = [&[][..], &[][..]];
+    let (i, _) = fill(terminated(digit1, tag(",")), &mut buf)(i)?;
+    Ok((i, buf))
+  }
+
+  assert_eq!(
+    fill_pair(b"123,456,"),
+    Ok((&b""[..], [&b"123"[..], &b"456"[..]]))
+  );
+  assert_eq!(
+    fill_pair(b"123,456,789"),
+    Ok((&b"789"[..], [&b"123"[..], &b"456"[..]]))
+  );
+  assert_eq!(
+    fill_pair(b"123,,"),
+    Err(Err::Error(error_position!(&b","[..], ErrorKind::Digit)))
+  );
+}
+
 fn issue_1459_clamp_capacity() {
   use nom::character::complete::char;
 
