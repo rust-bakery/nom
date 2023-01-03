934b80ee2960436e4c7cbce7e952cf7e6600d1aa
Geoffroy Couprie <contact@geoffroycouprie.com>
2023-01-03 21:24:50 +0100

    add streaming benchmarks (#1609)

---
+++ benchmarks/benches/http_streaming.rs
@@ -0,0 +1,199 @@
+#![cfg_attr(rustfmt, rustfmt_skip)]
+
+#[global_allocator]
+static ALLOC: jemallocator::Jemalloc = jemallocator::Jemalloc;
+
+use criterion::*;
+use nom::{IResult, bytes::streaming::{tag, take_while1}, character::streaming::{line_ending, char}, multi::many};
+
+#[cfg_attr(rustfmt, rustfmt_skip)]
+#[derive(Debug)]
+struct Request<'a> {
+  method:  &'a [u8],
+  uri:     &'a [u8],
+  version: &'a [u8],
+}
+
+#[derive(Debug)]
+struct Header<'a> {
+  name: &'a [u8],
+  value: Vec<&'a [u8]>,
+}
+
+#[cfg_attr(rustfmt, rustfmt_skip)]
+#[cfg_attr(feature = "cargo-clippy", allow(match_same_arms))]
+fn is_token(c: u8) -> bool {
+  match c {
+    128..=255 => false,
+    0..=31    => false,
+    b'('      => false,
+    b')'      => false,
+    b'<'      => false,
+    b'>'      => false,
+    b'@'      => false,
+    b','      => false,
+    b';'      => false,
+    b':'      => false,
+    b'\\'     => false,
+    b'"'      => false,
+    b'/'      => false,
+    b'['      => false,
+    b']'      => false,
+    b'?'      => false,
+    b'='      => false,
+    b'{'      => false,
+    b'}'      => false,
+    b' '      => false,
+    _         => true,
+  }
+}
+
+fn not_line_ending(c: u8) -> bool {
+  c != b'\r' && c != b'\n'
+}
+
+fn is_space(c: u8) -> bool {
+  c == b' '
+}
+
+fn is_not_space(c: u8) -> bool {
+  c != b' '
+}
+fn is_horizontal_space(c: u8) -> bool {
+  c == b' ' || c == b'\t'
+}
+
+fn is_version(c: u8) -> bool {
+  c >= b'0' && c <= b'9' || c == b'.'
+}
+
+fn request_line(input: &[u8]) -> IResult<&[u8], Request<'_>> {
+  let (input, method) = take_while1(is_token)(input)?;
+  let (input, _) = take_while1(is_space)(input)?;
+  let (input, uri) = take_while1(is_not_space)(input)?;
+  let (input, _) = take_while1(is_space)(input)?;
+  let (input, version) = http_version(input)?;
+  let (input, _) = line_ending(input)?;
+
+  Ok((input, Request {method, uri, version}))
+}
+
+fn http_version(input: &[u8]) -> IResult<&[u8], &[u8]> {
+  let (input, _) = tag("HTTP/")(input)?;
+  let (input, version) = take_while1(is_version)(input)?;
+
+  Ok((input, version))
+}
+
+fn message_header_value(input: &[u8]) -> IResult<&[u8], &[u8]> {
+  let (input, _) = take_while1(is_horizontal_space)(input)?;
+  let (input, data) = take_while1(not_line_ending)(input)?;
+  let (input, _) = line_ending(input)?;
+
+  Ok((input, data))
+}
+
+fn message_header(input: &[u8]) -> IResult<&[u8], Header<'_>> {
+  let (input, name) = take_while1(is_token)(input)?;
+  let (input, _) = char(':')(input)?;
+  let (input, value) = many(1.., message_header_value)(input)?;
+
+  Ok((input, Header{ name, value }))
+}
+
+fn request(input: &[u8]) -> IResult<&[u8], (Request<'_>, Vec<Header<'_>>)> {
+  let (input, req) = request_line(input)?;
+  let (input, h) = many(1.., message_header)(input)?;
+  let (input, _) = line_ending(input)?;
+
+  Ok((input, (req, h)))
+}
+
+
+fn parse(data: &[u8]) -> Option<Vec<(Request<'_>, Vec<Header<'_>>)>> {
+  let mut buf = &data[..];
+  let mut v = Vec::new();
+  loop {
+    match request(buf) {
+      Ok((b, r)) => {
+        buf = b;
+        v.push(r);
+
+        if b.is_empty() {
+
+          //println!("{}", i);
+          break;
+        }
+      }
+      Err(e) => {
+        println!("error: {:?}", e);
+        return None;
+      },
+    }
+  }
+
+  Some(v)
+}
+
+/*
+#[bench]
+fn small_test(b: &mut Bencher) {
+  let data = include_bytes!("../../http-requests.txt");
+  b.iter(||{
+    parse(data)
+  });
+}
+
+#[bench]
+fn bigger_test(b: &mut Bencher) {
+  let data = include_bytes!("../../bigger.txt");
+  b.iter(||{
+    parse(data)
+  });
+}
+*/
+
+fn one_test(c: &mut Criterion) {
+  let data = &b"GET / HTTP/1.1
+Host: www.reddit.com
+User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:15.0) Gecko/20100101 Firefox/15.0.1
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8
+Accept-Language: en-us,en;q=0.5
+Accept-Encoding: gzip, deflate
+Connection: keep-alive
+
+"[..];
+
+  let mut http_group = c.benchmark_group("http");
+  http_group.throughput(Throughput::Bytes(data.len() as u64));
+  http_group.bench_with_input(
+    BenchmarkId::new("parse_streaming", data.len()),
+     data,
+      |b, data| {
+    b.iter(|| parse(data).unwrap());
+  });
+
+  http_group.finish();
+}
+
+/*
+fn main() {
+  let mut contents: Vec<u8> = Vec::new();
+
+  {
+    use std::io::Read;
+
+    let mut file = File::open(env::args().nth(1).expect("File to read")).expect("Failed to open file");
+
+    let _ = file.read_to_end(&mut contents).unwrap();
+  }
+
+  let buf = &contents[..];
+  loop {
+    parse(buf);
+  }
+}
+*/
+
+criterion_group!(http_streaming, one_test);
+criterion_main!(http_streaming);
---
+++ benchmarks/benches/json_streaming.rs
@@ -0,0 +1,225 @@
+#[macro_use]
+extern crate criterion;
+
+#[global_allocator]
+static ALLOC: jemallocator::Jemalloc = jemallocator::Jemalloc;
+
+use criterion::Criterion;
+use nom::{
+  branch::alt,
+  bytes::streaming::{tag, take},
+  character::streaming::{anychar, char, multispace0, none_of},
+  combinator::{map, map_opt, map_res, value, verify},
+  error::{ErrorKind, ParseError},
+  multi::{fold, separated_list0},
+  number::streaming::{double, recognize_float},
+  sequence::{delimited, preceded, separated_pair},
+  IResult, Parser,
+};
+
+use std::collections::HashMap;
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum JsonValue {
+  Null,
+  Bool(bool),
+  Str(String),
+  Num(f64),
+  Array(Vec<JsonValue>),
+  Object(HashMap<String, JsonValue>),
+}
+
+fn boolean(input: &str) -> IResult<&str, bool> {
+  alt((value(false, tag("false")), value(true, tag("true"))))(input)
+}
+
+fn u16_hex(input: &str) -> IResult<&str, u16> {
+  map_res(take(4usize), |s| u16::from_str_radix(s, 16))(input)
+}
+
+fn unicode_escape(input: &str) -> IResult<&str, char> {
+  map_opt(
+    alt((
+      // Not a surrogate
+      map(verify(u16_hex, |cp| !(0xD800..0xE000).contains(cp)), |cp| {
+        cp as u32
+      }),
+      // See https://en.wikipedia.org/wiki/UTF-16#Code_points_from_U+010000_to_U+10FFFF for details
+      map(
+        verify(
+          separated_pair(u16_hex, tag("\\u"), u16_hex),
+          |(high, low)| (0xD800..0xDC00).contains(high) && (0xDC00..0xE000).contains(low),
+        ),
+        |(high, low)| {
+          let high_ten = (high as u32) - 0xD800;
+          let low_ten = (low as u32) - 0xDC00;
+          (high_ten << 10) + low_ten + 0x10000
+        },
+      ),
+    )),
+    // Could probably be replaced with .unwrap() or _unchecked due to the verify checks
+    std::char::from_u32,
+  )(input)
+}
+
+fn character(input: &str) -> IResult<&str, char> {
+  let (input, c) = none_of("\"")(input)?;
+  if c == '\\' {
+    alt((
+      map_res(anychar, |c| {
+        Ok(match c {
+          '"' | '\\' | '/' => c,
+          'b' => '\x08',
+          'f' => '\x0C',
+          'n' => '\n',
+          'r' => '\r',
+          't' => '\t',
+          _ => return Err(()),
+        })
+      }),
+      preceded(char('u'), unicode_escape),
+    ))(input)
+  } else {
+    Ok((input, c))
+  }
+}
+
+fn string(input: &str) -> IResult<&str, String> {
+  delimited(
+    char('"'),
+    fold(0.., character, String::new, |mut string, c| {
+      string.push(c);
+      string
+    }),
+    char('"'),
+  )(input)
+}
+
+fn ws<'a, O, E: ParseError<&'a str>, F: Parser<&'a str, O, E>>(f: F) -> impl Parser<&'a str, O, E> {
+  delimited(multispace0, f, multispace0)
+}
+
+fn array(input: &str) -> IResult<&str, Vec<JsonValue>> {
+  delimited(
+    char('['),
+    ws(separated_list0(ws(char(',')), json_value)),
+    char(']'),
+  )(input)
+}
+
+fn object(input: &str) -> IResult<&str, HashMap<String, JsonValue>> {
+  map(
+    delimited(
+      char('{'),
+      ws(separated_list0(
+        ws(char(',')),
+        separated_pair(string, ws(char(':')), json_value),
+      )),
+      char('}'),
+    ),
+    |key_values| key_values.into_iter().collect(),
+  )(input)
+}
+
+fn json_value(input: &str) -> IResult<&str, JsonValue> {
+  use JsonValue::*;
+
+  alt((
+    value(Null, tag("null")),
+    map(boolean, Bool),
+    map(string, Str),
+    map(double, Num),
+    map(array, Array),
+    map(object, Object),
+  ))(input)
+}
+
+fn json(input: &str) -> IResult<&str, JsonValue> {
+  ws(json_value).parse(input)
+}
+
+fn json_bench(c: &mut Criterion) {
+  let data = "  { \"a\"\t: 42,
+  \"b\": [ \"x\", \"y\", 12 ,\"\\u2014\", \"\\uD83D\\uDE10\"] ,
+  \"c\": { \"hello\" : \"world\"
+  }
+  }  ;";
+
+  // println!("data:\n{:?}", json(data));
+  c.bench_function("json streaming", |b| {
+    b.iter(|| json(data).unwrap());
+  });
+}
+
+fn recognize_float_bytes(c: &mut Criterion) {
+  println!(
+    "recognize_float_bytes result: {:?}",
+    recognize_float::<_, (_, ErrorKind)>(&b"-1.234E-12;"[..])
+  );
+  c.bench_function("recognize float bytes streaming", |b| {
+    b.iter(|| recognize_float::<_, (_, ErrorKind)>(&b"-1.234E-12;"[..]));
+  });
+}
+
+fn recognize_float_str(c: &mut Criterion) {
+  println!(
+    "recognize_float_str result: {:?}",
+    recognize_float::<_, (_, ErrorKind)>("-1.234E-12;")
+  );
+  c.bench_function("recognize float str streaming", |b| {
+    b.iter(|| recognize_float::<_, (_, ErrorKind)>("-1.234E-12;"));
+  });
+}
+
+fn float_bytes(c: &mut Criterion) {
+  println!(
+    "float_bytes result: {:?}",
+    double::<_, (_, ErrorKind)>(&b"-1.234E-12;"[..])
+  );
+  c.bench_function("float bytes streaming", |b| {
+    b.iter(|| double::<_, (_, ErrorKind)>(&b"-1.234E-12"[..]));
+  });
+}
+
+fn float_str(c: &mut Criterion) {
+  println!(
+    "float_str result: {:?}",
+    double::<_, (_, ErrorKind)>("-1.234E-12;")
+  );
+  c.bench_function("float str streaming", |b| {
+    b.iter(|| double::<_, (_, ErrorKind)>("-1.234E-12;"));
+  });
+}
+
+use nom::Err;
+use nom::ParseTo;
+fn std_float(input: &[u8]) -> IResult<&[u8], f64, (&[u8], ErrorKind)> {
+  match recognize_float(input) {
+    Err(e) => Err(e),
+    Ok((i, s)) => match s.parse_to() {
+      Some(n) => Ok((i, n)),
+      None => Err(Err::Error((i, ErrorKind::Float))),
+    },
+  }
+}
+
+fn std_float_bytes(c: &mut Criterion) {
+  println!(
+    "std_float_bytes result: {:?}",
+    std_float(&b"-1.234E-12;"[..])
+  );
+  c.bench_function("std_float bytes streaming", |b| {
+    b.iter(|| std_float(&b"-1.234E-12;"[..]));
+  });
+}
+
+criterion_group!(
+  benches,
+  json_bench,
+  recognize_float_bytes,
+  recognize_float_str,
+  float_bytes,
+  std_float_bytes,
+  float_str
+);
+criterion_main!(benches);
