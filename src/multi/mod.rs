81bfd82db72cbc9ebf8dccfa0d530eb445716d51
Geoffroy Couprie <contact@geoffroycouprie.com>
2023-01-15 20:15:00 +0100

    fix other uses of MAX_INITIAL_CAPACITY_BYTES
    fix  panic when the output type is zero sized

--- src/multi/mod.rs
+++ src/multi/mod.rs
@@ -392,7 +392,8 @@ where
       return Err(Err::Failure(E::from_error_kind(input, ErrorKind::ManyMN)));
     }
 
-    let max_initial_capacity = MAX_INITIAL_CAPACITY_BYTES / crate::lib::std::mem::size_of::<O>();
+    let max_initial_capacity =
+      MAX_INITIAL_CAPACITY_BYTES / crate::lib::std::mem::size_of::<O>().max(1);
     let mut res = crate::lib::std::vec::Vec::with_capacity(min.min(max_initial_capacity));
     for count in 0..max {
       let len = input.input_len();
